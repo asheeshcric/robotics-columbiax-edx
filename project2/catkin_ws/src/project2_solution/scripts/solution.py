1bfa1dc42c9314686b965c5a1b4534a3354bad00
Ashish <ashiz2013@gmail.com>
2018-10-09 12:22:22 +0545

    Complete Project 2 - Visualizing Transformations - Need to refactor the code though on solution.py

--- project2/catkin_ws/src/project2_solution/scripts/solution.py
+++ project2/catkin_ws/src/project2_solution/scripts/solution.py
@@ -7,6 +7,7 @@ import tf
 import tf2_ros
 import geometry_msgs.msg
 
+initial_loop = True
 
 def transform_coordinate_frame(T):
     new_frame = geometry_msgs.msg.Transform()
@@ -22,6 +23,16 @@ def transform_coordinate_frame(T):
     return new_frame
 
 
+def matrix_multiplication(matrix, vector):
+    vector.append(1)
+    return [sum([vector[x]*matrix[n][x] for x in range(len(vector))]) for n in range(len(matrix))]
+
+
+def between_angle(v1, v2):
+    unit_v1 = v1 / np.linalg.norm(v1)
+    unit_v2 = v2 / np.linalg.norm(v2)
+    return np.arccos(np.clip(np.dot(unit_v1, unit_v2), -1.0, 1.0))
+
 def transform_object():
     object_transform = geometry_msgs.msg.TransformStamped()
     object_transform.header.stamp = rospy.Time.now()
@@ -33,6 +44,7 @@ def transform_object():
         tf.transformations.translation_matrix((0.0, 1.0, 1.0)))
     object_transform.transform = transform_coordinate_frame(T1)
     br.sendTransform(object_transform)
+    return T1
 
 
 def transform_robot():
@@ -46,9 +58,11 @@ def transform_robot():
         tf.transformations.translation_matrix((0.0, -1.0, 0.0)))
     robot_transform.transform = transform_coordinate_frame(T2)
     br.sendTransform(robot_transform)
+    return T2
 
 
-def transform_camera():
+def transform_camera(T1, T2):
+    global initial_loop, angle, x_axis, origin2_camera, origin2_robot
     camera_transform = geometry_msgs.msg.TransformStamped()
     camera_transform.header.stamp = rospy.Time.now()
     camera_transform.header.frame_id = "robot_frame"
@@ -65,17 +79,59 @@ def transform_camera():
     # Translation matrix with displacement on y-axis and z-axis
     D3 = tf.transformations.translation_matrix([0.0, 0.1, 0.1])
 
+    # We do not know the camera frame orientation so we don't rotate it at the first loop
+    if initial_loop:
+        initial_loop = False
+        R3 = tf.transformations.quaternion_matrix(tf.transformations.quaternion_from_euler(0, 0, 0))
+
+        # Concatenated transformation matrix
+        T3 = tf.transformations.concatenate_matrices(D3, R3)
+        print("Camera View transformation = {}".format(T3))
+
+        # Calculate the coordinated of origin from object_frame w.r.t camera_frame
+        # Origin is the translation part from base_frame to object_frame
+        origin2 = tf.transformations.translation_from_matrix(T1)   
+
+        # Origin referenced to robot_frame
+        origin2_robot = matrix_multiplication(tf.transformations.inverse_matrix(T2), origin2.tolist())
+        origin2_robot = origin2_robot[:(len(origin2_robot) - 1)]
+
+        # Origin2 referenced from camera_frame
+        origin2_camera = matrix_multiplication(
+            tf.transformations.inverse_matrix(T3), origin2_robot)
+        origin2_camera = origin2_camera[:(len(origin2_camera) - 1)]
+
+
+        # X-axis for camera frame viewed from its own frame
+        x_axis = [1, 0, 0]
+
+        # Angle difference between x-axis and the origin of object_frame
+        angle = between_angle(x_axis, origin2_camera)
+        print("Angle = {}".format(angle))
+
+    # Vector through which x-axis has to rotate
+    normal_vector = np.cross(x_axis, origin2_camera)
+
+    R3 = tf.transformations.quaternion_matrix(
+            tf.transformations.quaternion_about_axis(
+                angle, normal_vector))
+
+    T3 = tf.transformations.concatenate_matrices(D3, R3)
+
+    camera_transform.transform = transform_coordinate_frame(T3)
     br.sendTransform(camera_transform)
 
 
 def publish_transforms():
-    transform_object()
-    transform_robot()
-    transform_camera()
+    T1 = transform_object()
+    T2 = transform_robot()
+    transform_camera(T1, T2)
 
 
 if __name__ == '__main__':
     rospy.init_node('project2_solution')
+
+    initial_loop = True
 
     br = tf2_ros.TransformBroadcaster()
     rospy.sleep(0.5)
