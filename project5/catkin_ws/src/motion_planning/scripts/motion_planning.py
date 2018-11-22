98c95a640775a31b2b242147ab917f0c22e05263
Ashish <ashiz2013@gmail.com>
2018-11-22 14:26:02 +0545

    Complete project5

--- project5/catkin_ws/src/motion_planning/scripts/motion_planning.py
+++ project5/catkin_ws/src/motion_planning/scripts/motion_planning.py
@@ -18,6 +18,7 @@ import sensor_msgs.msg
 import tf
 import trajectory_msgs.msg
 
+
 def convert_to_message(T):
     t = geometry_msgs.msg.Pose()
     position = tf.transformations.translation_from_matrix(T)
@@ -28,35 +29,62 @@ def convert_to_message(T):
     t.orientation.x = orientation[0]
     t.orientation.y = orientation[1]
     t.orientation.z = orientation[2]
-    t.orientation.w = orientation[3]        
+    t.orientation.w = orientation[3]
     return t
+
 
 def convert_from_message(msg):
     R = tf.transformations.quaternion_matrix((msg.orientation.x,
                                               msg.orientation.y,
                                               msg.orientation.z,
                                               msg.orientation.w))
-    T = tf.transformations.translation_matrix((msg.position.x, 
-                                               msg.position.y, 
+    T = tf.transformations.translation_matrix((msg.position.x,
+                                               msg.position.y,
                                                msg.position.z))
-    return numpy.dot(T,R)
+    return numpy.dot(T, R)
+
 
 def convert_from_trans_message(msg):
     R = tf.transformations.quaternion_matrix((msg.rotation.x,
                                               msg.rotation.y,
                                               msg.rotation.z,
                                               msg.rotation.w))
-    T = tf.transformations.translation_matrix((msg.translation.x, 
-                                               msg.translation.y, 
+    T = tf.transformations.translation_matrix((msg.translation.x,
+                                               msg.translation.y,
                                                msg.translation.z))
-    return numpy.dot(T,R)
-   
-   
+    return numpy.dot(T, R)
+
+
+def get_distance(point1, point2):
+    return numpy.linalg.norm(numpy.subtract(point1, point2))
+
+
+def get_vector(point1, point2):
+    return numpy.subtract(point1, point2)
+
+
+def fetch_point_at_distance(closest_point, rand_point, factor):
+    temp_vector = get_vector(rand_point, closest_point)
+    vector = temp_vector / numpy.linalg.norm(temp_vector)
+    vector *= factor
+    vector = numpy.add(vector, closest_point)
+    return vector
+
+
+def get_max_num_points(point1, point2, step_size):
+    return max(numpy.ceil(numpy.true_divide(abs(get_vector(point1, point2)), step_size)))
+
+
+def fetch_closest_point(node_list, r):
+    dists = [get_distance(q_pos, r) for i, q_pos in enumerate(d["pos_configuration_space"] for d in node_list)]
+    minimum_dist_index = dists.index(min(dists))
+    closest_point = node_list[minimum_dist_index].get("pos_configuration_space")
+    return minimum_dist_index, closest_point
+
 
 class MoveArm(object):
 
     def __init__(self):
-        print "Motion Planning Initializing..."
         # Prepare the mutex for synchronization
         self.mutex = Lock()
 
@@ -65,13 +93,20 @@ class MoveArm(object):
         self.num_joints = 7
         self.q_min = []
         self.q_max = []
-        self.q_min.append(-3.1459);self.q_max.append(3.1459)
-        self.q_min.append(-3.1459);self.q_max.append(3.1459)
-        self.q_min.append(-3.1459);self.q_max.append(3.1459)
-        self.q_min.append(-3.1459);self.q_max.append(3.1459)
-        self.q_min.append(-3.1459);self.q_max.append(3.1459)
-        self.q_min.append(-3.1459);self.q_max.append(3.1459)
-        self.q_min.append(-3.1459);self.q_max.append(3.1459)
+        self.q_min.append(-3.1459)
+        self.q_max.append(3.1459)
+        self.q_min.append(-3.1459)
+        self.q_max.append(3.1459)
+        self.q_min.append(-3.1459)
+        self.q_max.append(3.1459)
+        self.q_min.append(-3.1459)
+        self.q_max.append(3.1459)
+        self.q_min.append(-3.1459)
+        self.q_max.append(3.1459)
+        self.q_min.append(-3.1459)
+        self.q_max.append(3.1459)
+        self.q_min.append(-3.1459)
+        self.q_max.append(3.1459)
         # How finely to sample each joint
         self.q_sample = [0.05, 0.05, 0.05, 0.1, 0.1, 0.1, 0.1]
         self.joint_names = ["lwr_arm_0_joint",
@@ -83,7 +118,7 @@ class MoveArm(object):
                             "lwr_arm_6_joint"]
 
         # Subscribes to information about what the current joint values are.
-        rospy.Subscriber("/joint_states", sensor_msgs.msg.JointState, 
+        rospy.Subscriber("/joint_states", sensor_msgs.msg.JointState,
                          self.joint_states_callback)
 
         # Subscribe to command for motion planning goal
@@ -91,44 +126,42 @@ class MoveArm(object):
                          self.move_arm_cb)
 
         # Publish trajectory command
-        self.pub_trajectory = rospy.Publisher("/joint_trajectory", trajectory_msgs.msg.JointTrajectory, 
-                                              queue_size=1)        
+        self.pub_trajectory = rospy.Publisher("/joint_trajectory", trajectory_msgs.msg.JointTrajectory,
+                                              queue_size=1)
 
         # Initialize variables
         self.joint_state = sensor_msgs.msg.JointState()
 
         # Wait for moveit IK service
         rospy.wait_for_service("compute_ik")
-        self.ik_service = rospy.ServiceProxy('compute_ik',  moveit_msgs.srv.GetPositionIK)
-        print "IK service ready"
+        self.ik_service = rospy.ServiceProxy('compute_ik', moveit_msgs.srv.GetPositionIK)
 
         # Wait for validity check service
         rospy.wait_for_service("check_state_validity")
-        self.state_valid_service = rospy.ServiceProxy('check_state_validity',  
+        self.state_valid_service = rospy.ServiceProxy('check_state_validity',
                                                       moveit_msgs.srv.GetStateValidity)
-        print "State validity service ready"
 
         # Initialize MoveIt
         self.robot = moveit_commander.RobotCommander()
         self.scene = moveit_commander.PlanningSceneInterface()
         self.group_name = "lwr_arm"
-        self.group = moveit_commander.MoveGroupCommander(self.group_name) 
-        print "MoveIt! interface ready"
+        self.group = moveit_commander.MoveGroupCommander(self.group_name)
 
         # Options
         self.subsample_trajectory = True
-        print "Initialization done."
 
     def get_joint_val(self, joint_state, name):
         if name not in joint_state.name:
-            print "ERROR: joint name not found"
+            print
+            "ERROR: joint name not found"
             return 0
         i = joint_state.name.index(name)
         return joint_state.position[i]
 
     def set_joint_val(self, joint_state, q, name):
         if name not in joint_state.name:
-            print "ERROR: joint name not found"
+            print
+            "ERROR: joint name not found"
         i = joint_state.name.index(name)
         joint_state.position[i] = q
 
@@ -136,9 +169,10 @@ class MoveArm(object):
     our arm's set of joints in a particular order and returns a list q[] containing just 
     those values.
     """
+
     def q_from_joint_state(self, joint_state):
         q = []
-        for i in range(0,self.num_joints):
+        for i in range(0, self.num_joints):
             q.append(self.get_joint_val(joint_state, self.joint_names[i]))
         return q
 
@@ -147,8 +181,9 @@ class MoveArm(object):
     a particular order and edits the joint_state data structure to set the values 
     to the ones passed in.
     """
+
     def joint_state_from_q(self, joint_state, q):
-        for i in range(0,self.num_joints):
+        for i in range(0, self.num_joints):
             self.set_joint_val(joint_state, q[i], self.joint_names[i])
 
     """ This function will perform IK for a given transform T of the end-effector. It 
@@ -156,6 +191,7 @@ class MoveArm(object):
     the left arm, ordered from proximal to distal. If no IK solution is found, it 
     returns an empy list.
     """
+
     def IK(self, T_goal):
         req = moveit_msgs.srv.GetPositionIKRequest()
         req.ik_request.group_name = self.group_name
@@ -177,6 +213,7 @@ class MoveArm(object):
     one that is free of collisions. The values in q[] are assumed to be values for 
     the joints of the left arm, ordered from proximal to distal. 
     """
+
     def is_state_valid(self, q):
         req = moveit_msgs.srv.GetStateValidityRequest()
         req.group_name = self.group_name
@@ -187,17 +224,88 @@ class MoveArm(object):
         req.robot_state.joint_state = current_joint_state
         res = self.state_valid_service(req)
         return res.valid
-        
-     
-        
+
+    def fetch_rand_point(self):
+        return [random.uniform(self.q_min[i], self.q_max[i]) for i in range(self.num_joints)]
+
+    def discrete_path(self, closest_point, target_point):
+        step_size = self.q_sample
+        max_num_points = get_max_num_points(target_point, closest_point, step_size)
+
+        m_vector = numpy.true_divide(get_vector(target_point, closest_point), max_num_points - 1)
+        b_vector = numpy.true_divide(numpy.subtract(numpy.multiply(closest_point, max_num_points), target_point),
+                                     max_num_points - 1)
+        t_vector = [m_vector * i + b_vector for i in range(int(max_num_points) + 1)]
+        return t_vector
+
+    def is_path_collision_free(self, closest_point, target_point):
+        path = self.discrete_path(closest_point, target_point)
+
+        for row in path:
+            if self.is_state_valid(row) is False:
+                return False
+        return True
+
     def motion_plan(self, q_start, q_goal, q_min, q_max):
-        
         # Replace this with your code
-        q_list = [q_start, q_goal]
+        rrt_node_object = {
+            "pos_configuration_space": q_start,
+            "par_node": -1
+        }
+        rrt_node_list = [rrt_node_object.copy()]
+
+        max_nodes = 150
+        max_time = 240
+        start = rospy.get_rostime().secs
+        current_time = rospy.get_rostime().secs
+
+        while (len(rrt_node_list) < max_nodes) or ((current_time - start) < max_time):
+            rand_point = self.fetch_rand_point()
+
+            minimum_index_dist, closest_point = fetch_closest_point(rrt_node_list, rand_point)
+
+            target_point = fetch_point_at_distance(closest_point, rand_point, 0.5)
+
+            if self.is_path_collision_free(closest_point, target_point) is True:
+                rrt_node_object.update({"pos_configuration_space": target_point})
+                rrt_node_object.update({"par_node": minimum_index_dist})
+                rrt_node_list.append(rrt_node_object.copy())
+
+                if self.is_path_collision_free(target_point, q_goal) is True:
+                    par_node = len(rrt_node_list) - 1
+                    rrt_node_object.update({"par_node": par_node})
+                    rrt_node_object.update({"pos_configuration_space": q_goal})
+                    rrt_node_list.append(rrt_node_object.copy())
+                    break
+                else:
+                    pass
+            else:
+                pass
+
+            current_time = rospy.get_rostime().secs
+
+        q_list = [q_goal]
+        par_node = rrt_node_list[-1].get("par_node")
+
+        while True:
+            q_list.insert(0, rrt_node_list[par_node].get("pos_configuration_space"))
+            if par_node <= 0:
+                break
+            else:
+                par_node = rrt_node_list[par_node].get("par_node")
+
+        copy_q_list = [q_list[0]]
+
+        for i in range(len(q_list) - 2):
+
+            if self.is_path_collision_free(q_list[i], q_list[i + 2]) is False:
+                copy_q_list.append(q_list[i])
+
+        copy_q_list.append(q_list[-1])
+        q_list = copy_q_list
 
         return q_list
-    
-    
+
     def create_trajectory(self, q_list, v_list, a_list, t):
         joint_trajectory = trajectory_msgs.msg.JointTrajectory()
         for i in range(0, len(q_list)):
@@ -228,21 +336,17 @@ class MoveArm(object):
         T = convert_from_trans_message(msg)
         self.mutex.acquire()
         q_start = self.q_from_joint_state(self.joint_state)
-        print "Solving IK"
         q_goal = self.IK(T)
-        if len(q_goal)==0:
-            print "IK failed, aborting"
+        if len(q_goal) == 0:
             self.mutex.release()
             return
-        print "IK solved, planning"
         trajectory = self.project_plan(numpy.array(q_start), q_goal, self.q_min, self.q_max)
         if not trajectory.points:
-            print "Motion plan failed, aborting"
+            pass
         else:
-            print "Trajectory received with " + str(len(trajectory.points)) + " points"
             self.execute(trajectory)
         self.mutex.release()
-        
+
     def joint_states_callback(self, joint_state):
         self.mutex.acquire()
         self.joint_state = joint_state
@@ -250,6 +354,7 @@ class MoveArm(object):
 
     def execute(self, joint_trajectory):
         self.pub_trajectory.publish(joint_trajectory)
+
 
 if __name__ == '__main__':
     moveit_commander.roscpp_initialize(sys.argv)
